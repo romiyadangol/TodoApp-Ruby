b1bbdd907ea2dd89aaa07a41d733217d48a3444f
romiyadangol <dangolromiya@gmail.com>
2024-07-30 12:16:19 +0545

    Added listing

--- todo.rb
+++ todo.rb
@@ -4,9 +4,12 @@ class Todo
     end
 
     def add_todo(todo)
-        # @todos << todo
-        @todos.push(todo)
+        @todos << todo
         puts "Added new task successfully"
+    end
+
+    def list_todo
+        @todos
     end
 end
 
@@ -19,23 +22,29 @@ todo_hash = {
     ]
 }
 
-#new instance
-todo_app = Todo.new
+# new instance
+todo_app = Todo.new(todo_hash)
 
+loop do
+    puts "Todo- App"
+    puts "1. Add Todo List"
+    puts "2. List Todo"
 
-puts "Todo- App"
-puts "1. Add Todo List"
+    puts "Enter your choice: "
+    choice = gets.chomp.to_i
 
-puts "Enter your choice: "
-choice = gets.chomp.to_i
-
-case choice
-    when 1
-        puts "Enter task: "
-        task = gets.chomp.to_i
-        todo_app.add_todo({
-            isCompleted: false,
-            task: task
-        })
+    case choice
+        when 1
+            puts "Enter task: "
+            task = gets.chomp
+            todo_app.add_todo({
+                id: todo_app.list_todo.length + 1,
+                isCompleted: false,
+                task: task
+            })
+        when 2
+            puts todo_app.list_todo
+        else
+            puts "Invalid choice"
+    end
 end
-
